bd48d93147c203cb2251580b52e7eb14b3181833
Comfort5642 <comfort.ndungu.w@gmail.com>
2025-02-26 17:04:13 +0300

    Simple Calculator

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+num1=float(input("Enter first number: "))
+
+num2=float(input("Enter second number: "))
+
+sum_result=num1+num2
+
+print(f"sum:{sum_result}")
+
+difference_result=num1-num2
+
+print(f"difference:{difference_result}")
+
+product_result=num1*num2
+
+print(f"product:{product_result}")
+
+quotient_result=num1/num2
+
+print(f"quotient:{quotient_result}")
